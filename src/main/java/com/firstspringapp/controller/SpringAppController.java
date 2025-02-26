45252957721fabc990b97523551d55f6564d038b
TikeshKardate <tikesh.kardate@gmail.com>
2025-02-26 14:37:22 +0530

    [Tikesh] Add. Spring App Hello program added

---
+++ src/main/java/com/firstspringapp/controller/SpringAppController.java
@@ -0,0 +1,15 @@
+package com.firstspringapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringAppController {
+
+    //UC1.1 Method to print the Hello
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello from BridgeLabz";
+    }
+
+}
